75e6e759fd3f534d9622c28c3c037eb1c61afc64
minoinoue <phungtuandat29052004@gmail.com>
2025-07-21 18:35:46 +0700

    spring app

---
+++ src/main/java/com/tuandat/antifraudwp/controller/RegistrationController.java
@@ -0,0 +1,54 @@
+package com.tuandat.antifraudwp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tuandat.antifraudwp.model.MyAppUser;
+import com.tuandat.antifraudwp.model.MyAppUserRepository;
+import com.tuandat.antifraudwp.service.EmailService;
+import com.tuandat.antifraudwp.utils.JwtTokenUtil;
+
+@RestController
+public class RegistrationController {
+    
+	@Autowired
+	private MyAppUserRepository myAppUserRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@PostMapping (value = "/req/signup", consumes = "application/json")
+	public ResponseEntity<String> createUser(@RequestBody MyAppUser user) {
+		
+		MyAppUser existingAppUser = myAppUserRepository.findByEmail(user.getEmail());
+		
+		if(existingAppUser != null) {
+			if(existingAppUser.isVerified()) {
+				return new ResponseEntity<>("User Already exsit and verified.", HttpStatus.BAD_REQUEST);
+			} else {
+				String verificationToken = JwtTokenUtil.generateToken(user.getEmail());
+				existingAppUser.setVerficationToken(verificationToken);
+				myAppUserRepository.save(existingAppUser);
+				//Send email code
+				emailService.sendVerificationEmail(user.getEmail(), verificationToken);
+				return new ResponseEntity<>("Verification Email resent. Check your inbox", HttpStatus.OK);
+			}
+		}
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		String verificationToken = JwtTokenUtil.generateToken(user.getEmail());
+		user.setVerficationToken(verificationToken);
+		myAppUserRepository.save(user);
+		//Send email code
+		emailService.sendVerificationEmail(user.getEmail(), verificationToken);
+		
+		return new ResponseEntity<>("Registration successfull! Please verify your email", HttpStatus.OK);
+	}
+}
---
+++ src/test/java/com/tuandat/antifraudwp/AntifraudwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandat.antifraudwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntifraudwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
